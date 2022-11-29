1b2e0ceb6533311f3751b3dc052c9e4d695f21a8
felipematheus1337 <lipehbr@gmail.com>
2022-11-29 20:27:36 -0300

    Entidade Customer criada!

---
+++ src/models/Customer.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('customer')
+export class Customer {
+
+
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({type:'text',nullable:false})
+    name:string;
+
+    @Column({type:'text',nullable:true})
+    phone:string;
+
+    @Column({type:'text',nullable:false})
+    address:string;
+
+    @Column({type:'number'})
+    loyalty:number;
+
+
+
+}
